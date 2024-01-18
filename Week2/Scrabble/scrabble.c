8a53ef34d315e70be15e7bac53d19ee12b1a7e54
realpolya <123517550+realpolya@users.noreply.github.com>
2024-01-18 21:57:28 +0000

    Thu, Jan 18, 2024, 4:57 PM -05:00

---
+++ Week2/Scrabble/scrabble.c
@@ -0,0 +1,22 @@
+// playing the game of scrabble
+#include <cs50.h>
+#include <stdio.h>
+
+// MAIN GOAL:
+// each letter has a value, the program sums up
+// all the values in a word
+// to give the player a score
+
+int main(void)
+
+{
+    // Prompt player 1 for their word
+    string s = get_string("Player 1:");
+
+    // Prompt player 2 for their word
+
+    // TODO: Calculate number of years until we reach threshold
+
+    // TODO: Print number of years
+    //printf("Years: %i\n", y);
+}
