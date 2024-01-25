334dc968cfe7afc78914aaba0d4268f694e0c501
realpolya <123517550+realpolya@users.noreply.github.com>
2024-01-25 14:56:45 -0500

    Update caesar.c

--- Week2/caesar.c
+++ Week2/caesar.c
@@ -6,21 +6,34 @@
 #include <string.h>
 #include <math.h>
 
+bool only_digits(string argv[]);
+char rotate(char c, int n);
+
 int main(int argc, string argv[])
 {
-    //prompt the user to insert the key
+    //prompt the user to insert the key / make sure that just 2 total command line arguments
     if (argc != 2)
     {
-        printf("Usage: ./caesar need a number\n");
+        printf("Usage: ./caesar key\n");
         return 1;
     }
 
-    //have to convert the string to integer for numbers
-    if (isalpha(argv[argc-1])) //????
+    //(argv[1] must be a digit)
+    int digits = only_digits(argv);
+    if (digits != 0)
     {
-        printf("Usage: ./caesar need a number\n");
+        printf("Usage: ./caesar key\n");
         return 1;
     }
+
+    //convert string to integer
+    int key = atoi(argv[1]);
+    printf("%i\n", key);
+
+    //if (isdigit(key)) //????
+    //{
+    //    printf("bravo!\n");
+    //}
 
     //get plaintext from user
     string text = get_string("plaintext:  ");
@@ -32,12 +45,36 @@ int main(int argc, string argv[])
     printf("\n");
 
     //convert it to ciphertext
+    char ciphertext = rotate(*text, key);
 
     //print the resulting ciphertext
 
 }
 
-//int cipher_text(string argv);
-//{
-//    printf("%i\n", doub[0]);
-//}
+
+
+bool only_digits(string argv[])
+{
+    string keystring = argv[1];
+    int length = strlen(keystring);
+    int digits_answer = 0;
+    for (int y = 0; y < length; y++)
+    {
+        if (isdigit(keystring[y]))
+        {
+            digits_answer = digits_answer + 0;
+        }
+        else
+        {
+            digits_answer = digits_answer + 1;
+        }
+    }
+    return digits_answer;
+}
+
+char rotate(char c, int n)
+{
+    int i = c - 'a';
+    printf("%i\n", i);
+    return i;
+}
